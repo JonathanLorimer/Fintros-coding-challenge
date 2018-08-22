11341f1b0d0f8339431a1050b3a03eb39bf63396
Jonathan Lorimer <jonathan_lorimer@mac.com>
2018-08-21 20:26:52 -0400

    getting going on the UI

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ const requestPromise = require('request-promise-native')
 
 // app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.sendfile('index.html', { root: "./public/index.html" } );
 });
 app.post('/scrape', function(req, res){
+	console.log(req.body)
     Promise.all(
         req.body.map(story => {
             if (story.url){
+				console.log(story.url)
            		return requestPromise.get(story.url)
                     .then(e => {
 						let imageURL = e.match(/<meta property="og:image"\scontent="(\S+)"/g)
-						if (imageURL) story.image = imageURL[0]
+						if (imageURL) story.image = imageURL[0].slice(35, imageURL[0].length - 1)
 						else story.image = null
 						return story
 					})
@@ -38,5 +40,7 @@ app.post('/scrape', function(req, res){
     })
     .catch(err => console.log(err))
 });
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,5 @@
 
-
+const randomImg = 'https://picsum.photos/200/300/?random'
 const baseURL = 'https://hacker-news.firebaseio.com/v0/'
 
 const getResources = async () => {
@@ -9,13 +9,8 @@ const getResources = async () => {
 	
 	const top30data = await Promise.all( top30.map(x => fetch(`${baseURL}item/${x}.json`).then(y => y.json())) )
 	console.log(top30data)
-	// const pic = await fetch(`/scrape/${top30data[2].url}`, {method: 'GET'})
-	// const fetchData = JSON.stringify(top30data.reduce((acc, { url, title, by }, i) => {
-	// 	acc[i] = {url, title, by}
-	// 	return acc
-	// }, {}))
 	const fetchData = JSON.stringify(top30data)
-	const pic = await fetch('/scrape', {
+	const getPictures = await fetch('/scrape', {
 		method: 'POST', 
 		body: fetchData,
 		headers: {
@@ -23,14 +18,40 @@ const getResources = async () => {
       		'Content-Type': 'application/json'
         },
 	})
-	const pic2 = await pic.json()
-	// const pic = await fetch(`/scrape/hi`, {method: 'GET'})
-	
-	// const picReadable = await pic.text()
-	console.log(pic2)
-	// const imagesAdded = await Promise.all( top30data.map(x => fetch(x.url).then(y => y.json)))
-
+	const top30finalized = await getPictures.json()
+	console.log(top30finalized)
+	const top30even = top30finalized.filter((e, i) => (i + 2 % 2 !== 0) && e )
+	const top30odd = top30finalized.filter((e, i) => (i + 2 % 2 === 0) && e)
+	if(document.getElementById('filter-even').checked){
+		heroStory(top30even[6])
+	} else {
+		heroStory(top30odd[0])
+	}
 
 }
 
-// getResources()
+const string = "<meta property='og:image' content='"
+console.log(string.length)
+
+const heroStory = (item) => {
+	let link = document.getElementsByClassName("hacker-news__hero-story--link")[0]
+	let image = document.getElementsByClassName("hacker-news__hero-story--image")[0]
+	let title = document.getElementsByClassName("hacker-news__hero-story--title")[0]
+	let text = document.getElementsByClassName("hacker-news__hero-story--text")[0]
+	console.log(link)
+	console.log(image)
+	console.log(title)
+	console.log(text)
+	console.log(item)
+	link.setAttribute('href', item.url);
+	if(item.image) {
+		image.setAttribute('src', item.image)
+	}
+	else image.setAttribute('src', 'https://picsum.photos/1260/710/?random');
+	title.innerHTML = item.title
+	if(item.text){
+		text.innerHTML = item.text
+	}
+}
+
+getResources()
